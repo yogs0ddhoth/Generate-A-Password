78ec00c11838e7fb47c49db5db14dea6090ced1f
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-01-13 10:18:14 -0500

    updated js with pseudocode

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   console.log("Hey! You clicked the button!")
 
+  /* 
+  open prompts { 
+    series of password criteria {
+      choseLength. length of 8 <= 128 characters;
+    }
+      
+    confirm characterType
+    [lowercase, uppercase, mumeric, special char];
+    
+    validate charType -> log var?  
+    
+  }
+  */
 
   return "Generated Password"
 }
