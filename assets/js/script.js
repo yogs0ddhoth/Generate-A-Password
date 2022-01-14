fda08dc03cf9b808cd27c9a1b596676a90bc9efd
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-01-14 16:00:16 -0500

    updated js and linked in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 
 var generateBtn = document.querySelector("#generate");
 
+var charsUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charsLower = "abcdefghijklmnopqrstuvwxyz";
+var charsNumeric = "0123456789";
+var charsSpecial = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
 function generatePassword() {
-  console.log("Hey! You clicked the button!")
+  console.log("Hey! You clicked the button!");
 
   /* 
   open prompts { 
@@ -17,6 +22,34 @@ function generatePassword() {
     
   }
   */
+  var passwordLength = prompt("Choose your password length \n(Choose a number between 8 and 128)");
+  console.log(passwordLength);
+
+  var charBank = "";
+
+  var confirmUpper = window.confirm("Do you want your password to include Uppercase Letters?");
+  if (confirmUpper) {
+    charBank += charsUpper;
+    console.log(charBank);
+  }
+ 
+  var confirmLower = window.confirm("Do you want your password to include Lowercase Letters?");
+  if (confirmLower) {
+    charBank += charsLower;
+    console.log(charBank);
+  }
+
+  var confirmNumeric = window.confirm("Do you want your password to include Numbers?");
+  if (confirmNumeric) {
+    charBank += charsNumeric;
+    console.log(charBank);
+  }
+
+  var confirmSpecial = window.confirm("Do you want your password to include Special Characters?");
+  if (confirmSpecial) {
+    charBank += charsSpecial;
+    console.log(charBank);
+  }
 
   return "Generated Password"
 }
